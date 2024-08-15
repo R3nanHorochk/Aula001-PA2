98385b7fb539c63dc48da5d62a59a006ab50aec6
R3nanHorochk <renanhorochkdeandrade@gmail.com>
2024-08-15 00:49:54 +0000

    Ano de fundação da FCI

--- aula001.c
+++ aula001.c
@@ -7,6 +7,6 @@ int main(){
   printf("Mackenzie - Fci \n");
   printf("Ciencia da Computação \n ");
   printf("Algoritmos e Programação II \n");
-
+  printf("A FCI foi fundada no ano de 1970 \n");
   return 0;
 }
