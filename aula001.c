699b2d258f39ea9d5186f9094a58885e932919b7
R3nanHorochk <renanhorochkdeandrade@gmail.com>
2024-08-15 00:48:01 +0000

    Inclusão do nome do Curso

--- aula001.c
+++ aula001.c
@@ -1,10 +1,12 @@
-
-include  <stdio.h>
+/*Lab 1 - Exemplo 1
+*/
+#include  <stdio.h>
 
 int main(){
+  //Imprime mensagens
   printf("Mackenzie - Fci \n");
-  printf("Algoritmos e");
-  printf("Prog. || \n");
+  printf("Ciencia da Computação \n ");
+  printf("Algoritmos e Programação II \n");
 
   return 0;
 }
